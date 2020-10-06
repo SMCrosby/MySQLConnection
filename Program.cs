bf70ebf91205e12ca51efd4135c7d535cfb07e4d
Sarah Crosby <SarahCroMich@gmail.com>
2020-10-06 16:19:40 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using MySQLLibrary;     //add namespace for library
+
+namespace MySQLConnection {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Working on sending SQLServer data to C# library");
+
+            var ss = new SqlServer();
+            var ok = ss.Connect("localhost", "sqlexpress", "EdDb");        //localhost instead of server name (pulls up my laptops info)
+
+            var students = ss.ExecuteQuery("SELECT * from Student;");
+
+            ss.Disconnect();
+
+
+        }
+    }
+}
